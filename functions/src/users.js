c6e12ac7c249b0ada11765448b5544b935c971e5
john32y <john.thelronge@gmail.com>
2023-07-17 16:49:27 -0400

    "send jwt"

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,4 +1,6 @@
+import  jwt from 'jsonwebtoken'
 import { db } from './dbConnect.js'
+import { secret } from '../cred.js'
 
 const coll = db.collection('users')
 
@@ -6,16 +8,14 @@ export async function signup(req, res) {
   const {email, password } = req.body 
   // not a secure way to store password
   await coll.insertOne({ email: email.toLowerCase(), password })
+  login(req, res)
 
 }
 
 export async function login(req, res) {
-  const { email, password }m= req.body
+  const { email, password } = req.body
   let user = await coll.findOne({ email: email.toLowerCase(), password})
   delete user.password // strip out password
-  // TODO: create token and sendt with user below
-  res.send({ user })
-
-
-
+  const token = jwt.sign(user, secret)
+  res.send({ user, token })
 }
