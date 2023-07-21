55d573cf36e279eb210b2c072c93520cab14732c
john32y <john.thelronge@gmail.com>
2023-07-21 10:49:07 -0400

    added hash password

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,23 +1,31 @@
 import  jwt from 'jsonwebtoken'
+import { hash } from 'bcrypt'
 import { ObjectId } from 'mongodb'
 import { db } from './dbConnect.js'
-import { secret } from '../cred.js'
+import { secret, salt } from '../cred.js'
 
 const coll = db.collection('users')
 
 export async function signup(req, res) {
   const {email, password } = req.body 
-  // not a secure way to store password
-  await coll.insertOne({ email: email.toLowerCase(), password })
+  // TODO: HASH password
+  const hashedPassword = await hash(password, salt)
+  await coll.insertOne({ email: email.toLowerCase(), password: hashedPassword })
+  // not checking if email already exists or doing any validation
   login(req, res)
 
 }
 
 export async function login(req, res) {
   const { email, password } = req.body
-  let user = await coll.findOne({ email: email.toLowerCase(), password})
+  const hashedPassword = await hash(password, salt)
+  let user = await coll.findOne({ email: email.toLowerCase(), password: hashedPassword})
+  if(!user) {
+    res.status(401).send({ message: 'Invalid email or password'})
+    return
+  }
   delete user.password // strip out password
-  const token = jwt.sign(user, secret)
+  const token = jwt.sign(user, secret) // ENCODE
   res.send({ user, token })
 }
 
